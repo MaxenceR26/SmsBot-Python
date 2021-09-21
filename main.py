250d838b032067aeaf80306e9448ea10436b3da0
Katsuhi-Dev <maxencedu62_@outlook.fr>
2021-09-21 22:01:38 +0200

    Sms Bot

---
+++ main.py
@@ -0,0 +1,41 @@
+import vonage
+
+# Vonage API !
+
+# Grace à cette outils tu peux envoyer des messages, à n'importe qui tout en étant inconnu :P
+
+# By Maxence.R
+
+class BotMessage:
+    def __init__(self):
+        self.client = vonage.Client(key='TonApiKey', secret='TaCléSecrete')
+        self.sms = vonage.Sms(self.client)
+
+        self.name = None
+        self.number = None
+        self.message = None
+        self.Data = None
+
+    def SendMessage(self):
+        self.name = input("Name : ")
+        self.number = input("Number of phone : ")
+        self.message = input("Message : ")
+
+        self.Data = self.sms.send_message(
+            {
+                "from": self.name,
+                "to": self.number,
+                "text": self.message,
+            }
+        )
+
+        self.Verification()
+
+    def Verification(self):
+        if self.Data["messages"][0]["status"] == "0":
+            print("Message sent successfully.")
+        else:
+            print(f"Message failed with error: {self.Data['messages'][0]['error-text']}")
+
+
+BotMessage().SendMessage()
